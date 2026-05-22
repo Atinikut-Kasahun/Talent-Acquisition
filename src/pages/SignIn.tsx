95fffe7a1a66f18acdf13c37788ff3bada1989d2
Atinikut Kasahun <256632353+Atinikut-Kasahun@users.noreply.github.com>
2026-05-22 13:11:10 +0300

    fixed api

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = import.meta.env.VITE_API_URL + "/api";
 
 export default function SignIn() {
   const navigate = useNavigate();
